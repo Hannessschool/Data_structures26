dd7685485824b0a53cc6776b6b37a9f610008291
Hannessschool <eerolaha@arcada.fi>
2026-01-24 13:30:14 +0200

    Lesson 1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,16 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello!\n");
+        System.out.println("Hello!\n");
+
+
+        Car car = new Car("Volkswagen");
+        Car car2 = new Car("Tesla");
+
+        System.out.println(car.getName());
+        car.setName("Volvo");
+        System.out.println(car.getName());
+        System.out.println(car2.getName());
     }
+
 }
